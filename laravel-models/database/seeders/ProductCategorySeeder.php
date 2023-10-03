a0480ca9da896ed6dc366773560c5153da6adad0
timeabardi <146115048+timeabardi@users.noreply.github.com>
2023-10-03 14:59:53 +0200

    ora

---
+++ laravel-models/database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\ProductCategory;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ProductCategory::factory()
+        ->create([
+            'name' => 'Category 3',
+        ]);
+
+        ProductCategory::insert([
+            'name' => 'Category 2',
+        ]);
+    }
+}
