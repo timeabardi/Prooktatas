bc6777eb91eb0e79d3b7b91a4d6a4fef98375d62
timeabardi <146115048+timeabardi@users.noreply.github.com>
2023-10-14 12:12:50 +0200

    feat:laravel-vite-sikerult

--- laravel-vite/resources/views/welcome.blade.php
+++ laravel-vite/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="container">
@@ -16,6 +17,5 @@
             </div>
         </div>
     </div>
-    @vite(['resources/js/app.js'])
 </body>
 </html>
