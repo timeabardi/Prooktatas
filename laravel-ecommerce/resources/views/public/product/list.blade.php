2cc37b443988b2a853d689ceec666cd7e773f00c
timeabardi <146115048+timeabardi@users.noreply.github.com>
2023-10-14 11:50:04 +0200

    feat:laravel-vite

--- laravel-ecommerce/resources/views/public/product/list.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('public.layout.app')
-
-@section('content')
-<section>
-<div>
-    <h1>Products</h1>
-</div>
-</section>
-@endsection
---
+++ laravel-vite/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-md-4 mx-auto">
+                <div class="btn btn-danger">
+                    Hello There
+                </div>
+            </div>
+        </div>
+    </div>
+    @vite(['resources/js/app.js'])
+</body>
+</html>
