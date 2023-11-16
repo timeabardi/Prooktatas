68a67bb309e04c7e53ce1c6d8ff18a5110a2ea0e
timeabardi <146115048+timeabardi@users.noreply.github.com>
2023-11-16 13:04:40 +0100

    feat: laravel install, migrate events, seed user

---
+++ laravel-exam/database/migrations/2023_11_16_125607_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 255);
+            $table->string('type');
+            $table->string('desc');
+            $table->datetime('event_start_at')->nullable();
+            $table->datetime('published_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
---
+++ laravel-exam/database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('12Pass')
+        ]);
+    }
+}
